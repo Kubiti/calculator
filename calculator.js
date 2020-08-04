329f85d1d00da7049063a809a3eac2b329f84704
Kubiti <kubitisam18@gmail.com>
2020-08-04 17:17:20 +0000

    Class modifications

--- calculator.js
+++ calculator.js
@@ -1,49 +1,60 @@
 
-  const
-  buttons = document.getElementsByTagName("button"),
-  screen = document.getElementById('screen');
-  
+//   const
+//   buttons = document.getElementsByTagName("button"),
+//   screen = document.getElementById('screen');
 
-Array.from(buttons).forEach(button =>
-  button.addEventListener("click", showNumber));
 
-function showNumber(event) {
-  const button = event.target;
-  if(screen.innerHTML==0){
-    screen.innerHTML=button.value;
-  }
-   else if (button.value != 5 ) { screen.innerHTML += button.value; }
-  else  {screen.innerHTML +=5 ;}
-}
+// Array.from(buttons).forEach(button =>
+//   button.addEventListener("click", showNumber));
 
-  const add=document.getElementById('add'),
-        multiply=document.getElementById('mulltiply'),
-        subtract=document.getElementById('subtract'),
-        divide=document.getElementById('divide'),
-        clear=document.getElementById('clear');
-        function doMath(){
-          if(button.value=='='){
-            screen.innerHTML=Number.parseFloat(button.value);
-          }
-        }
-        /*
-        buttons = document.getElementsByTagName("button"),
-        screen = document.getElementById('screen');
+// function showNumber(event) {
+//   const button = event.target;
+//   if(screen.innerHTML==0){
+//     screen.innerHTML=button.value;
+//   }
+//    else if (button.value != 5 ) { screen.innerHTML += button.value; }
+//   else  {screen.innerHTML +=5 ;}
+// }
+
+//   const add=document.getElementById('add'),
+//         multiply=document.getElementById('mulltiply'),
+//         subtract=document.getElementById('subtract'),
+//         divide=document.getElementById('divide'),
+//         clear=document.getElementById('clear');
+//         function doMath(){
+//           if(button.value=='='){
+//             screen.innerHTML=Number.parseFloat(button.value);
+//           }
+//         }
+        
+
+        
+        
         class calculator {
-          constructor(previousContent, presentContent){
-            this.previousContent= screen;
-            this.presentContent=presentContent;
+          
+
+          constructor(buttons, screen){
+            this.buttons= document.getElementsByTagName("button");
+            this.screen = document.getElementById('screen');
           }
-          showNumber(){
-            if (buttons.value!=6){
-              screen.innerHTMl=buttons.value;
-            }
+          showNumber(button){
+            
+            if(this.screen.innerHTML==0){
+                   this.screen.innerHTML=button.value;
+                }
+                 else if (button.value != 5 ) { this.screen.innerHTML += button.value; }
+                else  {this.screen.innerHTML +=5 ;}
+              }
+          doSomething() {
+            Array.from(this.buttons).forEach(button =>
+              button.addEventListener("click",() =>{
+                this.showNumber(button);
+            }));
           }
+          
+
         }
         
-        Array.from(buttons).forEach(button =>
-          button.addEventListener("click",() =>{
-            calculator.showNumber;
-
-          }));*/
-        
+        const newCalculator = new calculator()
+        newCalculator.doSomething();
+  
